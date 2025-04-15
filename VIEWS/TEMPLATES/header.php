8e6df316b0d69bd2b4c62556b120f2d4e9d6a7da
helenamMm <helenamm.001@gmail.com>
2025-04-14 20:49:17 -0600

    acuerdate de modificar el slq en traer datos usuarios y contra

--- VIEWS/TEMPLATES/header.php
+++ VIEWS/TEMPLATES/header.php
@@ -4,31 +4,32 @@
     </div>
     <nav class="nav-opciones">
         <ul>
-            <?php
-      if (isset($_SESSION['usuario'])) {
-        echo '
-                    <li><a href="usuario">Mi Perfil</a></li>';
+        <?php
 
-        // Solo para administradores
-        if ($_SESSION['usuario']['usuario_administrador'] == 1) {
-          echo '<li><a href="reportes">Reportes</a></li>';
-        }
-
-        echo '
-                    <li><a href="mensajes">Chats</a></li>
-                    <li><a href="contactos">Contactos</a></li>
-                    <li><a href="pagina_principal">Feed</a></li>
-                    <li><a href="mis_publicaciones">Mis Publicaciones</a></li>
-                    <li><a href="inicio_sesion">Cerrar sesión</a></li>
-                ';
-      } else {
-        // Usuario sin sesión
-        echo '
+          $usuario = $_SESSION['usuario'] ?? null;
+          if (!$usuario) {
+    
+            echo '
                     <li><a href="inicio_sesion">Iniciar Sesión</a></li>
                     <li><a href="registro_usuario">Registrarse</a></li>
                 ';
-      }
-      ?>
+          }else{
+            echo '
+            <li><a href="usuario">Mi Perfil</a></li>';
+            //print_r($usuario['usuario_administrador']);
+            if ($usuario['usuario_administrador'] == 1) {
+              echo '<li><a href="reportes">Reportes</a></li>';
+            }
+  
+              echo '
+              <li><a href="mensajes">Chats</a></li>
+              <li><a href="contactos">Contactos</a></li>
+              <li><a href="pagina_principal">Feed</a></li>
+              <li><a href="mis_publicaciones">Mis Publicaciones</a></li>
+              <li><a href="inicio_sesion">Cerrar sesión</a></li>
+        ';
+          } 
+        ?>
         </ul>
     </nav>
 </header>
